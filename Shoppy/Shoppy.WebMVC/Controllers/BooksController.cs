89cedee738c7a967780da7bfa19eb6286b677f88
Blue Teeth <daominhtri1000@gmail.com>
2024-05-17 17:52:45 +0700

    Add template products page MVC

---
+++ Shoppy/Shoppy.WebMVC/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shoppy.WebMVC.Controllers;
+
+public class BooksController : Controller
+{
+    private ILogger<BooksController> _logger;
+
+    public BooksController(ILogger<BooksController> logger)
+    {
+        _logger = logger;
+    }
+
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Detail()
+    {
+        return View();
+    }
+}
