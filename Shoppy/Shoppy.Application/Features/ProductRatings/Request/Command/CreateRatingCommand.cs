4019a210f2d172ed94031186f1427d6c48f246e8
Blue Teeth <daominhtri1000@gmail.com>
2024-05-17 14:09:36 +0700

    Add template MVC

---
+++ Shoppy/Shoppy.Application/Features/ProductRatings/Request/Command/CreateRatingCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shoppy.Application.Features.ProductRatings.Request.Command;
+
+public record CreateRatingCommand
+{
+    [Required] [Range(0.5, 5)] public int RateValue { get; set; }
+
+    [StringLength(250)] public string? Comment { get; set; }
+
+    public Guid ProductId { get; set; }
+}
