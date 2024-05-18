1ef783fd17dba8b9fa1e7d2f722ed37bb9bea267
Blue Teeth <daominhtri1000@gmail.com>
2024-05-18 13:49:39 +0700

    Update mvc template UI

---
+++ Shoppy/Shoppy.WebMVC/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shoppy.WebMVC.Controllers;
+
+public class CartController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult CheckOut()
+    {
+        return View();
+    }
+}
+
